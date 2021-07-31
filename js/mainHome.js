c18e6df0a12a29705adf8478f8cbdb28f272ea16
juosta <79445286+juosta@users.noreply.github.com>
2021-07-31 13:19:33 +0300

    back to top btn

--- js/mainHome.js
+++ js/mainHome.js
@@ -24,3 +24,20 @@ document.addEventListener("scroll", () => fillPorgress(skillsData));
 const projects = new Projects('#projects', projectsData);
 
 
+//back to top button
+var mybutton = document.getElementById("topbtn");
+mybutton.addEventListener("click", topFunction);
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
